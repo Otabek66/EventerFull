dafd82e8226da3015d917e8d9128a29f375b9282
AbdusamadMansurov <abdusamat.mansurov@gmail.com>
2022-09-19 11:18:23 +0500

    user (getAll, getOne)

--- src/main/java/com/example/event/controller/ClientController.java
+++ src/main/java/com/example/event/controller/ClientController.java
@@ -1,11 +1,12 @@
 package com.example.event.controller;
 
 import com.example.event.dto.ApiResponse;
+import com.example.event.model.User;
 import com.example.event.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,11 @@ public class ClientController {
     @GetMapping
     public ResponseEntity<?> getAll() {
         ApiResponse<List<User>> response = userService.getAll();
+        return ResponseEntity.status(response.getStatus()).body(response);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOne(@PathVariable Long id){
+        ApiResponse<User> response = userService.getOne(id);
         return ResponseEntity.status(response.getStatus()).body(response);
     }
 
--- src/main/java/com/example/event/service/UserService.java
+++ src/main/java/com/example/event/service/UserService.java
@@ -1,20 +1,51 @@
 package com.example.event.service;
 
 import com.example.event.dto.ApiResponse;
+import com.example.event.model.User;
 import com.example.event.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import org.apache.catalina.User;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
-//    public ApiResponse<List<User>> getAll() {
-//
-//    }
+
+    public ApiResponse<List<User>> getAll() {
+        List<User> users = userRepository.findAll();
+        if (users.isEmpty()) {
+            return ApiResponse.<List<User>>builder().
+                    message("Users not found!!!").
+                    status(204).
+                    success(false).
+                    build();
+        }
+        return ApiResponse.<List<User>>builder().
+                message("Users here!!!").
+                status(200).
+                success(true).
+                data(users).
+                build();
+    }
+
+    public ApiResponse<User> getOne(Long id){
+        Optional<User> userOptional = userRepository.findById(id);
+        if (userOptional.isPresent()) {
+            return ApiResponse.<User>builder().
+                    message("User here!!!").
+                    status(200).
+                    success(true).
+                    data(userOptional.get()).
+                    build();
+        }
+        return ApiResponse.<User>builder().
+                message("User not found!!!").
+                status(204).
+                success(false).
+                build();
+    }
 }
